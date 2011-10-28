5348dab90a31521f96cfab1fbf3b1fde035ffd16
Andre Graf <andre@dergraf.org>
2011-10-28 09:31:13 +0200

    Forgot to delete nif-experiment

--- src/mydb_drv.erl
+++
@@ -1,9 +0,0 @@
--module(mydb_drv).
--export([foo/1, bar/1]).
--on_load(init/0).
-init() ->
-    ok = erlang:load_nif("./priv/mydb_drv", 0).
-foo(_X) ->
-    exit(nif_library_not_loaded).
-bar(_Y) ->
-    exit(nif_library_not_loaded).
